e78149ad45e8e591373f6fb0a5b8ec4a64fcd2ef
Yusuke Kawamoto <novogrammer@gmail.com>
2019-08-30 19:38:12 +0900

    update mass and force

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -36,12 +36,12 @@ export const CAT_MASS=1;
 export const CAT_SPAWN_RATE=1;
 export const CAT_MAX_QTY=100;
 
-export const MOM_CAT_MASS=1;
+export const MOM_CAT_MASS=8;
 
 export const MOUSE_VELOCITY_TO_FORCE=MOM_CAT_MASS/100;
 
 export const MOM_CAT_MAX_VELOCITY=10;
-export const MOM_CAT_WALK_FORCE=MOM_CAT_MASS*100;
+export const MOM_CAT_WALK_FORCE=MOM_CAT_MASS*20;
 export const MOM_CAT_SCALE=2;
 //size: Vector3 {x: 0.3, y: 0.625, z: 0.8} 
 export const MOM_CAT_FORCE_POINT=new THREE.Vector3(0,0,0.2).multiplyScalar(MOM_CAT_SCALE);
